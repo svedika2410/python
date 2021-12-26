4b4c72c42249c298f1af2c46e1f0a6fedca0ad4f
svedika2410 <63898114+svedika2410@users.noreply.github.com>
2021-12-26 22:56:34 +0530

    Update quizgame.py

--- quizgame.py
+++ quizgame.py
@@ -4,28 +4,44 @@ if playing != "yes":
     quit()
 
 print("Lets play!")
-score = 1
+score =1
+def checkscore(x):
+    if x<0:
+        print("Game over")
+        print("Exit")
+        exit()
+    
 
 answer = input("Are we humans?  ")
 if answer == "yes":
     print("Correct!")
     score += 1
+    checkscore(score)
 else:
     print("False")
     score -= 1
+    checkscore(score)
 answer = input("Do you study in college?  ")
 if answer == "yes":
     print("Correct!")
     score += 1
+    checkscore(score)
 else:
     print("False")
     score -= 1
+    checkscore(score)
 answer = input("Are you in AI?  ")
 if answer == "yes":
     print("Correct!")
     score += 1
+    checkscore(score)
 else:
     print("False")
     score -= 1
+    checkscore(score)
 
 print("Your score is:",score,"out of 3")
+
+    
+
+
